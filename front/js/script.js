cef17632fd02d527c2c2d323cada06b5e2c30422
QuentinTL <q.thibaut.lazaro96@gmail.com>
2023-03-09 21:35:08 +0100

    Update script.js
    modification du script.js sur conseils de Pascal afin d'afficher notre html créé.

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,16 @@
 // On récupere nos information depuis le fichier Product.js situé dans dossier back/models/
-const kanapData = fetch("./back/models/Product.js");
+// const kanapData = fetch("./back/models/Product.js");
 
 // const resultKanap = kanapData.json();
 
-const nosKanap = kanapData.products[0];
+// const nosKanap = kanapData.products[0];
+
+// Ici on choisit l'élément qui sera le parent de nos éléments créer ci-dessus
+const itemsParent = document.querySelector("#items");
 
 // On créer une balise a qui sera parente de nos élément et sera
 // elle meme enfant de notre section dans le html existant
-const linkItem = document.createElement("a"({href: "#"}));
+const linkItem = document.createElement("a");
 
 // On creer un article dans le quel seront nos éléments lui meme
 // enfant de notre balise a
@@ -15,26 +18,21 @@ const articleItem = document.createElement("article");
 
 // Création d'un élément image
 const imageElement = document.createElement("img");
-imageElement.src = products.imageUrl;
+// imageElement.src = products.imageUrl;
 
 // Création d'un élément h3
-const nameElement = document.createElement("h3"({class: "productName"}));
-nameElement.innerText = products.name;
+const nameElement = document.createElement("h3");
+// nameElement.innerText = products.name;
 
 // Création d'une description via l'élément p
-const descriptionElement = document.createElement(
-  "p"({class: "productDescription"}),
-);
-nameElement.innerText = products.name;
-
-// Ici on choisit l'élément qui sera le parent de nos éléments créer ci-dessus
-const itemsParent = document.querySelector("#items");
+const descriptionElement = document.createElement("p");
+// nameElement.innerText = products.name;
 
 // La on creer notre ordre d'apparition
 
-const anItem = itemsParent.appendChild(linkItem);
-const itemDetail = anItem.appendChild(articleItem);
+itemsParent.appendChild(linkItem);
+linkItem.appendChild(articleItem);
 
-itemDetail.appendChild(imageElement);
-itemDetail.appendChild(nameElement);
-itemDetail.appendChild(descriptionElement);
+articleItem.appendChild(imageElement);
+articleItem.appendChild(nameElement);
+articleItem.appendChild(descriptionElement);
