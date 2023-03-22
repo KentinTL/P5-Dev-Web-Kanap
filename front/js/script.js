655eb6c6a74cc4393f325dce0a4eeec2e34d39a5
QuentinTL <q.thibaut.lazaro96@gmail.com>
2023-03-22 22:45:59 +0100

    update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,38 +1,54 @@
 // On récupere nos information depuis le fichier Product.js situé dans dossier back/models/
-const reponse = await fetch("../../back/models/Product.js");
+const kanapData = fetch("http://localhost:3000/api/products/", {
+ method: "GET"
+}).then(function(response) {
 
-const pieces = await response.js();
+  return response.json();
 
-const article = pieces.products[0];
+}).then(function(products){
+  
+  console.log(products);
+  
+  // ICI on boucle sur notre tableau products afin de récupérer chaque product 
+  for(let i = 0; i < products.length; i++) {
+    const product = products[i];
+    // On créer une balise a qui sera parente de nos élément et sera
+    // elle meme enfant de notre section dans le html existant
+    const linkItem = document.createElement("a",({href: "#"}));
+    
+    // On creer un article dans le quel seront nos éléments lui meme
+    // enfant de notre balise a
+    const articleItem = document.createElement("article");
+    
+    // Création d'un élément image
+    const imageItem = document.createElement("img");
+    imageItem.src = product.imageUrl;
+    
+    // Création d'un élément h3
+    const nameItem = document.createElement("h3",({class: "productName"}));
+    nameItem.innerText = product.name;
+    
+    // Création d'une description via l'élément p
+    const descriptionItem = document.createElement(
+      "p",({class: "productDescription"}),
+      );
+      descriptionItem.innerText = product.description;
+      
+      // Ici on choisit l'élément qui sera le parent de nos éléments créer ci-dessus
+      const itemsParent = document.querySelector("#items");
 
-// Ici on choisit l'élément qui sera le parent de nos éléments créer ci-dessus
-const itemsParent = document.querySelector("#items");
+      // La on creer notre ordre d'apparition
+      articleItem.appendChild(imageItem);//on pousse notre image dans notre article
+      articleItem.appendChild(nameItem);//On pousse notre nom dans notre article
+      articleItem.appendChild(descriptionItem);//On pousse notre description dans notre article
+      
+      linkItem.appendChild(articleItem);//On pousse notre article et ses détails dans notre lien
+      
+      itemsParent.appendChild(linkItem);//Enfin on pousse notre lien dans la balise section qui le parent global
+    }
+      
+    }).catch(function(error){
 
-// On créer une balise a qui sera parente de nos élément et sera
-// elle meme enfant de notre section dans le html existant
-const linkItem = document.createElement("a");
+  console.log(error);
 
-// On creer un article dans le quel seront nos éléments lui meme
-// enfant de notre balise a
-const articleItem = document.createElement("article");
-
-// Création d'un élément image
-const imageElement = document.createElement("img");
-imageElement.src = article.imageUrl;
-
-// Création d'un élément h3
-const nameElement = document.createElement("h3");
-nameElement.innerText = article.name;
-
-// Création d'une description via l'élément p
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.name;
-
-// La on creer notre ordre d'apparition
-
-itemsParent.appendChild(linkItem);
-linkItem.appendChild(articleItem);
-
-articleItem.appendChild(imageElement);
-articleItem.appendChild(nameElement);
-articleItem.appendChild(descriptionElement);
+})
