1055321fa7e1f2ffe2b85c09b561f8340ab0815c
QuentinTL <q.thibaut.lazaro96@gmail.com>
2023-03-30 18:52:13 +0200

    afficher le produit Ok
    Nous affichons bien le produits sur lequel on a cliqué dans le l'index.html sur la page product.httml avec tous ces éléments

--- front/js/product.js
+++ front/js/product.js
@@ -1,43 +1,40 @@
-let parseUrl = new URL(window.location.href);//Récupération de l'URL
+let parseUrl = new URL(window.location.href); //Récupération de l'URL
 
-let id = parseUrl.searchParams.get('id');//attribution de la valeur id dans l'url à notre variable id
+let id = parseUrl.searchParams.get("id"); //attribution de la valeur id dans l'url à notre variable id
 
+fetch("http://localhost:3000/api/products/" + id)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (product) {
+    // Création de mes balise img et option nécessaire
+    let selectImgParent = document.querySelector(".item__img");
+    let imageItem = document.createElement("img");
+    imageItem.src = product.imageUrl;
+    imageItem.alt = product.altTxt;
+    selectImgParent.appendChild(imageItem);
 
-fetch("http://localhost:3000/api/products/")
-.then(function(response) {
-  
-  return response.json();
-  
-}).then(function(products){
-  products.forEach(product => {    
-    if (id === product._id) {
-      let kanapImg = product.imageUrl;
-      let imgAlt = product.altTxt;
-      let kanapName = product.name;
-      let kanapPrice = product.price;
-      let kanapDescription = product.description;
-      let kanapColors = product.colors;
+    // Récupération de la donnée name dans notre modeles/Product.js
+    let kanapName = document.getElementById("title");
+    kanapName.innerHTML = product.name;
 
-      for (let index = 0; index < kanapColors.length; index++) {//On boucle sur le tableau de couleurs
-        let kanapColor = kanapColors[index];
-        let optionColor = document.createElement('option');
-        optionColor.value = kanapColor;
-        optionColor.innerHTML = kanapColor;
-        let selectParents = document.getElementById("colors");
-        selectParents.appendChild(optionColor);
-        // document.getElementById('colors').innerHTML = '<option value="'+kanapColor+'">'+kanapColor+'</option>;'
-      }
-      
-      
-      // Création de mes balise img et option nécessaire
-      document.getElementsByClassName('item__img').innerHTML = '<img src="'+kanapImg+'"alt="'+imgAlt+'">';
-  
-      // Affichage des éléments qui ne sont pas à créer
-      document.getElementById("price").innerHTML=kanapPrice;
-      document.getElementById("title").innerHTML=kanapName;
-      document.getElementById("description").innerHTML=kanapDescription;
+    // Récupération de la donnée price dans notre modeles/Product.js
+    let kanapPrice = document.getElementById("price");
+    kanapPrice.innerHTML = product.price;
+
+    // Récupération de la donnée description dans notre modeles/Product.js
+    let kanapDescription = document.getElementById("description");
+    kanapDescription.innerHTML = product.description;
+    let kanapColors = product.colors;
+
+    for (let index = 0; index < kanapColors.length; index++) {
+      //On boucle sur le tableau de couleurs
+      let kanapColor = kanapColors[index];
+      let optionColor = document.createElement("option");
+      optionColor.value = kanapColor;
+      optionColor.innerHTML = kanapColor;
+      let selectParents = document.getElementById("colors");
+      selectParents.appendChild(optionColor);
+      // document.getElementById('colors').innerHTML = '<option value="'+kanapColor+'">'+kanapColor+'</option>;'
     }
   });
-
-
-});
