ed806e147fc1a2db5bac32fac6bc22050754f291
QuentinTL <q.thibaut.lazaro96@gmail.com>
2023-04-13 18:58:05 +0200

    Fin de la page product
    Encore quelques détails a revoir afin de bien comprendre et voir si on ne peut pas optimiser légèrement

--- front/js/product.js
+++ front/js/product.js
@@ -27,7 +27,6 @@ fetch("http://localhost:3000/api/products/" + id)
     kanapDescription.innerHTML = product.description;
     let kanapColors = product.colors;
 
-    
     for (let index = 0; index < kanapColors.length; index++) {
       //On boucle sur le tableau de couleurs
       let kanapColor = kanapColors[index];
@@ -38,7 +37,6 @@ fetch("http://localhost:3000/api/products/" + id)
       selectParents.appendChild(optionColor);
     }
 
-
     // Développement de l'envois des données en appuyant sur le bouton addToCart
     let addButton = document.getElementById("addToCart");
     addButton.addEventListener("click", function () {
@@ -48,26 +46,39 @@ fetch("http://localhost:3000/api/products/" + id)
         quantity: ("quantity", document.getElementById("quantity").value),
       };
 
-      let setkanapInfos = localStorage.setItem('kanapInfos', JSON.stringify(globalAdd));
       let getkanapInfos = JSON.parse(localStorage.getItem("kanapInfos"));
-      // besoin d'une conditions pour vérifier si la variable getkanapInfos existe
-      if (!getkanapInfos) {
-        // NE PAS OUBLIER de mettre des alertes en cas de non sélections d'une couleur et/ou d'un quantité
-        if (getkanapInfos.color === "") {
-          alert("Veuillez sélectionnez la couleur")
-        } 
-        if (getkanapInfos.quantity === "0" || getkanapInfos.quantity === "") {
-          alert("Veuillez sélectionnez la quantité")
+      // NE PAS OUBLIER de mettre des alertes en cas de non sélections d'une couleur et/ou d'un quantité
+      if (globalAdd.color === "") {
+        alert("Veuillez sélectionner une couleur");
+      } else if (globalAdd.quantity === "0" || globalAdd.quantity === "") {
+        alert("Veuillez sélectionner une quantité");
+      } else {
+        // besoin d'une conditions pour vérifier si la variable getkanapInfos existe
+        if (getkanapInfos) {
+          let searchDetails = getkanapInfos.find(
+            (e) => e.id === globalAdd.id && e.color === globalAdd.color,
+          );
+          if (searchDetails) {
+            let newQuantity =
+              parseInt(globalAdd.quantity) + parseInt(searchDetails.quantity);
+            searchDetails.quantity = newQuantity;
+            localStorage.setItem("kanapInfos", JSON.stringify(getkanapInfos));
+          } else {
+            getkanapInfos.push(globalAdd);
+            localStorage.setItem("kanapInfos", JSON.stringify(getkanapInfos));
+          }
+        } else {
+          getkanapInfos = [];
+          getkanapInfos.push(globalAdd);
+          localStorage.setItem("kanapInfos", JSON.stringify(getkanapInfos));
         }
-        
-        // Condition pour vérifier si dans notre localstorage il existe déjà un id de même couleurs
-        // Si id avec même couleur existe déjà incrémenter la quantité
-        
-        // Créer une variable qui sera = a getkanapInfos.find()
-        
-    }
+        alert("Félicitations vous avez envoyé un canapé dans votre panier");
+      }
 
+      // Condition pour vérifier si dans notre localstorage il existe déjà un id de même couleurs
+      // Si id avec même couleur existe déjà incrémenter la quantité
 
+      // Créer une variable qui sera = a getkanapInfos.find()
     });
   });
 // Infos Local Storage
