a478a51ba6697944c4fed1cff4ab3a29016f6774
QuentinTL <q.thibaut.lazaro96@gmail.com>
2023-04-22 00:22:09 +0200

    Mise à jour de cart.js
    Remise en ordre du code et affichage des bons éléments dans le cart.html

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,42 +1,122 @@
-fetch("http://localhost:3000/api/products/")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (products) {
-    // Ici on récupère les canapé du localStorage
-    let getkanapInfos = JSON.parse(localStorage.getItem("kanapInfos"));
+// Ici on récupère les Items avec la clef "kanapInfos" dans notre localStorage
+let getkanapInfos = JSON.parse(localStorage.getItem("kanapInfos"));
+console.log(getkanapInfos);
+function globalCart() {
+  // On vient vérifier qu'il y a bien des données dans notre LocalStorage
+  if(!getkanapInfos) {
+    // Si pas de données éxistentes alors :
+    console.log("Il n'y a rien dans le localStorage");
+    alert('Votre panier est vide')
+  } else {
+    // Si données existentes alors :
+    // On récupère le/les Item(s) qui est/sont dans le localStorage
+    console.log("Il y a des item dans le localStorage");
+    for (let c = 0; c < getkanapInfos.length; c++) {
+      // On stock dans une variable les données item par item 
+      const item = getkanapInfos[c];
+      // On vient lier les "Objets" de Products.js pour pouvoir comparer avec notre localStorage
+      fetch("http://localhost:3000/api/products/" + item.id)
+        .then(function(response) {
+        return response.json();
+      })
+      .then(function(products){
+        // Grace à notre item.id concaténer dans le fetch on récupère uniquement le/les Objet(s)
+        // du Product.js correspondant à/aux id de notre localStorage
+        console.log(products);
+        console.log(item.quantity);
+        // Maintenant on créer nos éléments html
 
-    for (let i = 0; i < getkanapInfos.length; i++) {
-      let myCart = getkanapInfos[i];
-      // Création de mes balise img et option nécessaire
-      let selectImgParent = document.querySelector(".cart__item__img");
-      let imageItem = document.createElement("img");
+        // Notre article
+        const articleItem = document.createElement("article");
+        articleItem.classList.add("cart__item");
+        articleItem.setAttribute("data-id", item.id);
+        articleItem.setAttribute("data-color", item.color);
 
-      let selectTextParent = document.querySelector(
-        ".cart__item__content__description",
-      );
-      let titleItem = document.createElement("h2");
-      let descriptionItem = document.createElement("p");
-      let priceItem = document.createElement("p");
+        // Notre div pour l'image
+        const divImg = document.createElement("div");
+        divImg.classList.add("cart__item__img");
+        articleItem.appendChild(divImg);
+        // Notre image de la div "cart__item__img"
+        const kanapImg = document.createElement("img");
+        kanapImg.src = products.imageUrl;
+        kanapImg.alt = products.altTxt;
+        divImg.appendChild(kanapImg)
 
-      let quantityItem = document.getElementById("123");
-      console.log(quantityItem.value);
-      quantityItem.value = myCart.quantity;
-      console.log(quantityItem.value);
+        // Notre div container pour "Nom, Couleur, Prix" puis "Quantité" et "Bouton Supprimé"
+        let contentItem = document.createElement("div");
+        contentItem.classList.add("cart__item__content")
+        articleItem.appendChild(contentItem);
 
-      products.forEach((product) => {
-        if (product._id === myCart.id) {
-          imageItem.src = product.imageUrl;
-          imageItem.alt = product.altTxt;
-          selectImgParent.appendChild(imageItem);
-          titleItem.innerHTML = product.name;
-          selectTextParent.appendChild(titleItem);
-          descriptionItem.innerHTML = product.description;
-          selectTextParent.appendChild(descriptionItem);
-          priceItem.innerHTML = product.price + " €";
-          selectTextParent.appendChild(priceItem);
-          quantityItem;
-        }
+        // Notre div pour "Nom, Couleur, Prix"
+        let descriptionItem = document.createElement("div");
+        descriptionItem.classList.add("cart__item__content__description");
+        let nameItem = document.createElement("h2");
+        nameItem.innerHTML = products.name;
+        let colorItem = document.createElement("p");
+        colorItem.innerHTML = item.color;
+        let priceItem = document.createElement("p");
+        priceItem.innerHTML = products.price + "€";
+        descriptionItem.appendChild(nameItem);
+        descriptionItem.appendChild(colorItem);
+        descriptionItem.appendChild(priceItem);
+        contentItem.appendChild(descriptionItem);
+        articleItem.appendChild(contentItem);
+
+        // Notre div pour "Quantité" et "Bouton Supprimer"
+        let settingsItem = document.createElement("div");
+        settingsItem.classList.add("cart__item__content__settings");
+        // Notre div pour la "Quantité"
+        let quantityItem = document.createElement("div");
+        quantityItem.classList.add("cart__item__content__settings__quantity")
+        let quantityItemTxt = document.createElement("p");
+        quantityItemTxt.innerHTML = "Qté : ";
+        quantityItem.appendChild(quantityItemTxt);
+        let quantityItemInput = document.createElement("input");
+        quantityItemInput.type = "number";
+        quantityItemInput.classList.add("itemQuantity");
+        quantityItemInput.name = "itemQuantity";
+        quantityItemInput.min = "1";
+        quantityItemInput.max = "100";
+        quantityItemInput.value = item.quantity;
+
+        quantityItem.appendChild(quantityItemTxt);
+        quantityItem.appendChild(quantityItemInput)
+        settingsItem.appendChild(quantityItem);
+
+        // Notre div pour le "Bouton Supprimer"
+        let settingsdeleteItem = document.createElement("div");
+        settingsdeleteItem.classList.add("cart__item__content__settings__delete")
+        let deleteItemP = document.createElement("p");
+        deleteItemP.classList.add("deleteItem")
+        deleteItemP.innerHTML = "Supprimer";
+        settingsdeleteItem.appendChild(deleteItemP);
+        settingsItem.appendChild(settingsdeleteItem);
+
+        // On ajoute notre div "Quantité" et "Bouton Supprimer" notre div article
+        articleItem.appendChild(settingsItem);
+
+        // On sélectionne le parent html dans lequel nos éléments créés seront affichés
+        const parentElement = document.querySelector("#cart__items");
+        parentElement.appendChild(articleItem);
+        
       });
+      // Maintenant on vient gérer les totaux "Quantité" et "Prix"
+      function calcQuantity() {
+        quantityByKanap = item.quantity;
+        quantityByKanapParsed = parseInt(quantityByKanap);
+        if (getkanapInfos.length === 1){
+          console.log("Il n'y a qu'un Item dans le localSotrage");
+          let totalQuantity = quantityByKanapParsed
+          console.log(totalQuantity);
+        }else if (getkanapInfos.length > 1) {
+          console.log("Il y a plus d'un Item dans le localStorage");
+          let totalQuantity = quantityByKanapParsed + quantityByKanapParsed;
+          console.log(totalQuantity);
+        }
+      }
+      calcQuantity()
     }
-  });
+  }
+}
+
+globalCart()
